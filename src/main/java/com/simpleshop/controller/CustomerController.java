c20489c8f25ec0b91055b574ac844adefe9bd63e
Shifal <shifal643@gmail.com>
2025-11-25 12:41:42 +0530

    added base url for the controllers

--- src/main/java/com/simpleshop/controller/CustomerController.java
+++ src/main/java/com/simpleshop/controller/CustomerController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/customers")
+@RequestMapping("${api.customer.base}")
 public class CustomerController {
 
     private final CustomerServiceInterface customerService;
